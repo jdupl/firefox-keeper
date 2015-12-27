0ed1c00766a6f4af7151c352dd32aff17e7132dd
Justin Duplessis <drfoliberg@gmail.com>
2015-12-27 15:06:59 -0500

    Complete parsing of nested bookmarks

--- client/client.py
+++ client/client.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python3
 import lz4
 import json
+import re
+
 
 def read_jsonlz4(filename):
     with open(filename, mode='rb') as f:
         if f.read(8) != b"mozLz40\0":
-            print("Bad firefox jsonlz4 file.")
             return
         raw_data = f.read()
         uncompressed = lz4.decompress(raw_data)
         return json.loads(uncompressed.decode('utf8'))
 
+
 def recurse(data):
+    bookmarks = {}
     for c in data:
         if 'title' in c:
-            print(c['title'])
-        if 'children' in c:
-            recurse(c['children'])
+            if 'uri' not in c and 'children' in c:
+                bookmarks[c['title']] = recurse(c['children'])
+            elif 'uri' in c and http_p.match(c['uri']):
+                bookmarks[c['title']] = c['uri']
+    return bookmarks
 
+
+http_p = re.compile('^http[s]?://')
 filename = "/home/justin/.mozilla/firefox/9shb6xj3.default/bookmarkbackups/bookmarks-2015-12-24_147_rDZlzXqiTshE+lOx9FDMxg==.jsonlz4"
 
 data = read_jsonlz4(filename)
@@ -25,8 +32,8 @@ if not data:
     print('Invalid bookmark file')
     exit(1)
 
+bookmarks = {}
 for c in data['children']:
-    if 'title' in c:
-        print(c['title'])
-    if 'children' in c:
-        recurse(c['children'])
+    if 'title' in c and c['title'] != '' and 'children' in c:
+        bookmarks[c['title']] = recurse(c['children'])
+print(json.dumps(bookmarks))

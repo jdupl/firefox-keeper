7cf3761e7ef3b5783cf6c46c8dc77e050c3a6f0b
Justin Duplessis <drfoliberg@gmail.com>
2015-12-30 18:54:54 -0500

    Assign bookmarks to workers

--- server/server.py
+++ server/server.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
 import json
+import multiprocessing as mp
 
 from flask import Flask, request, jsonify
+
+
+import time
 
 app = Flask(__name__)
 
 
-def read_bookmark(item, parents=[]):
-    print(item, parents)
+def read_bookmark(*args):
+    queue.put((args))
 
 
 def read(item, parents=[]):
     for k, v in item.items():
         if type(v) is str:
-            read_bookmark(v, parents)
+            read_bookmark(k, v, parents)
         else:
             parents.append(k)
             read(v, parents)
-    if len(parents) > 0:
-        parents.pop()
+            parents.pop()
 
 
 @app.route('/api', methods=['POST'])
@@ -34,7 +37,25 @@ def update():
     return jsonify({}), 200
 
 
+def handle(task):
+    print('handling ', task)
+    time.sleep(5)
+    print('done handling ', task)
+
+
+def safe_handle(queue):
+    while True:
+        task = queue.get(True)
+        try:
+            handle(task)
+        except Exception as e:
+            print("error: {} handling {}".format(e, task))
+
+
 def setup():
+    global queue
+    queue = mp.Queue()
+    pool = mp.Pool(5, safe_handle, (queue,))
     return app
 
 if __name__ == '__main__':

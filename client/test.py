65c7a79dbed241fb83a49bd7abf52599ae77454b
Felix Leblanc <felix.leblanc1305@gmail.com>
2016-01-03 16:27:54 -0500

    test(client): read_jsonlz4 with no mozilla header

--- client/test.py
+++ client/test.py
@@ -7,9 +7,16 @@ import lz4
 
 class ReadLZ4TestCase(unittest.TestCase):
 
-    def compress_fixture(self, fixture_lz4_path):
+    def compress_fixture(self, fixture_lz4_path, with_header):
         """
-        Compress json fixture with lz4 and with mozilla header
+        Compress json fixture with lz4
+
+        fixture_lz4_path:
+        The path to the compressed fixture
+
+        with_header:
+        Specify if you want to have the mozilla header
+        at the start of the compressed file
         """
         fixture_json_path = 'fixtures/fixture.json'
 
@@ -18,18 +25,30 @@ class ReadLZ4TestCase(unittest.TestCase):
             compressed_data = lz4.compress(data)
 
             with open(fixture_lz4_path, mode='wb') as lz4_file:
-                header = b'mozLz40\0'
-                lz4_file.write(header)
+                if with_header:
+                    header = b'mozLz40\0'
+                    lz4_file.write(header)
                 lz4_file.write(compressed_data)
 
     def test_read_jsonlz4(self):
         path = 'fixtures/fixture.lz4'
         expected = json.loads("{\"test\": \"test\"}")
-        self.compress_fixture(path)
+        self.compress_fixture(path, True)
 
         actual = client.read_jsonlz4(path)
 
         self.assertEqual(expected, actual)
+
+        os.remove(path)
+
+    def test_read_jsonlz4_no_mozilla_header(self):
+        path = 'fixtures/fixture.lz4'
+        expected = json.loads("{\"test\": \"test\"}")
+        self.compress_fixture(path, False)
+
+        actual = client.read_jsonlz4(path)
+
+        self.assertEqual(None, actual)
 
         os.remove(path)
 

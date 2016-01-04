074e8c04ace34c5f5df2f1c33a18870579a34960
Felix Leblanc <felix.leblanc1305@gmail.com>
2016-01-04 16:07:36 -0500

    test(server): Youtube handler is returned

--- server/test.py
+++ server/test.py
@@ -13,6 +13,13 @@ class ReadModFromUrl(unittest.TestCase):
 
         self.assertEqual("handlers.html_default", handler.__name__)
 
+    def test_find_mod_for_url_youtube(self):
+        url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        server.setup()
+
+        handler = server.find_mod_for_url(url)
+
+        self.assertEqual("handlers.youtube", handler.__name__)
 
 if __name__ == "__main__":
     unittest.main()

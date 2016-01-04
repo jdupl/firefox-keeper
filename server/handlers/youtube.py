885b9c5056989aaef382a1549438c0f21814ba04
Justin Duplessis <drfoliberg@gmail.com>
2016-01-04 17:58:10 -0500

    Implement youtube-dl handler

--- server/handlers/youtube.py
+++ server/handlers/youtube.py
@@ -1,10 +1,23 @@
 import re
+import os
+import subprocess
 
-pattern = re.compile('http[s]?://(www\.)?youtube\.com/watch\?v=(.*)')
+
+pattern = re.compile('http[s]?://(?:www\.)?youtube\.com/watch\?v=(.*)')
 
 
 def new(task):
-    return('youtube')
+    url = task[1]
+    dest = '/tmp/'
+
+    m = pattern.match(url)
+    youtube_id = m.groups()[0].split('&')[0]  # TODO Better regex
+
+    try:
+        subprocess.check_call(['youtube-dl', youtube_id, '-o',
+                              os.path.join(dest, '%(title)s-%(id)s.%(ext)s')])
+    except KeyboardInterrupt as e:
+        raise e
 
 
 def matches(url):
